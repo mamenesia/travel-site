04f0c0e2a5fab0716a5f6e8ac312e7d54f0abe5b
mamenesia <dedy.prasetyo.h@gmail.com>
2019-03-16 19:09:13 +0700

    Add autoprefixer to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-let gulp = require('gulp');
+let gulp = require('gulp'),
+		watch = require('gulp-watch'),
+		postcss = require('gulp-postcss'),
+		autoprefixer = require('autoprefixer'),
+		cssvars = require('postcss-simple-vars'),
+		nested = require('postcss-nested');
+
 
 gulp.task('default', function(){
 	console.log("Hooraay...")
-})
+});
+
+gulp.task('html', function(){
+	console.log('Imagine Something useful being done to your HTML file.');
+});
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/style.css')
+	.pipe(postcss([cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('style', function(){
+	gulp
+});
+
+gulp.task('watch', function(){
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
+	});
+});
